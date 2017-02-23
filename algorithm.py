7589d9f8f27cb88ac63943442a18a5244522b143
Thibault Févry <ThibaultFevry@gmail.com>
2017-02-23 20:37:32 +0100

    Working code

--- algorithm.py
+++ algorithm.py
@@ -2,22 +2,11 @@ from parser import Parser
 import numpy as np
 import pandas as pd
 
+
+
 parser = Parser('data/me_at_the_zoo.in')
 parser.parse()
 
-video = 0
-cache = 0
-score = 0
-# for endpoint in parser.endpoints:
-    # diff_lat = -1
-    # if cache in endpoint.latencies.keys():
-        # cache_lat = endpoint.latencies[cache]
-        # diff_lat = endpoint.dc_latency - cache_lat
-        # for req in parser.requests:
-            # if req.video == video and req.source == endpoint.index:
-                # score += diff_lat * req.qty
-
-# print(score)
 
 scores = np.zeros((parser.n_videos, parser.n_caches))
 scores = []
@@ -36,6 +25,20 @@ for cache in range(parser.n_caches):
         scores[cache].append(score)
 
 scores = [pd.Series(l).sort_values(ascending=False) for l in scores]
-print(scores[0])
+
+mega_string = str(parser.n_caches) + "\n"
+for i, cache in enumerate(scores):
+    weight = 0
+    to_add = []
+    for vid in cache.index:
+        if not (parser.video_sizes[vid] + weight < parser.cache_size):
+            break
+        to_add.append(vid)
+        weight += parser.video_sizes[vid]
+    mega_string += "{} {}\n".format(str(i), " ".join([str(i) for i in to_add]))
+with open('test.out', 'w') as out:
+    out.write(mega_string)
+
+
 # scores = pd.DataFrame(scores)
 # scores = scores.sort
